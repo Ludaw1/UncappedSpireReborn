f6b2e3175acecc9f8bd030d9b7a3af811a5df6cb
Nick Dawson <nickdawson1994@gmail.com>
2026-07-31 21:25:47 +1000

    whatever

--- UncappedSpireRebornCode/MainFile.cs
+++ UncappedSpireRebornCode/MainFile.cs
@@ -19,6 +19,8 @@ public partial class MainFile : Node
         //If you want to use scripts defined in your mod for Godot scenes, uncomment the following line.
         //Godot.Bridge.ScriptManagerBridge.LookupScriptsInAssembly(Assembly.GetExecutingAssembly());
 
+        UncappedUpgrades.UpgradeCapsConfig.Load();
+
         Harmony harmony = new(ModId);
 
         harmony.PatchAll();
---
+++ UncappedSpireRebornCode/UncappedUpgrades/CardModelPatches/Patch_MaxUpgradeLevel.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Cards;
+
+namespace UncappedSpireReborn.UncappedSpireRebornCode.UncappedUpgrades.CardModelPatches;
+
+// Step 1: remove the upgrade level cap entirely for every card.
+// No per-card curation yet - that comes later once this is confirmed working.
+[HarmonyPatch(typeof(CardModel), nameof(CardModel.MaxUpgradeLevel), MethodType.Getter)]
+public static class Patch_MaxUpgradeLevel
+{
+    public static void Postfix(CardModel __instance, ref int __result)
+    {
+        __result = int.MaxValue;
+    }
+}
